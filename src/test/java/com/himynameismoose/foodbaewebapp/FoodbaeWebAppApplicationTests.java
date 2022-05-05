43df105d531b12646d515990cd22709aac878b58
himynameismoose <mershellemae@gmail.com>
2022-05-05 16:52:37 -0700

    Initial commit

---
+++ src/test/java/com/himynameismoose/foodbaewebapp/FoodbaeWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.himynameismoose.foodbaewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodbaeWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
