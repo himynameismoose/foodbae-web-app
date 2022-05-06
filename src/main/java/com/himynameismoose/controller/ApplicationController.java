beeea56716e24f77cd9b4adcec49091d4e5714ba
himynameismoose <mershellemae@gmail.com>
2022-05-06 16:49:32 -0700

    moved to controller folder

--- src/main/java/com/himynameismoose/controller/ApplicationController.java
+++ src/main/java/com/himynameismoose/controller/ApplicationController.java
@@ -1,12 +1,19 @@
-package com.himynameismoose.foodbaewebapp;
+package com.himynameismoose.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * This is the Application controller
+ * @RestController annotation handles requests
+ *
+ * @author mershellerivera
+ * @version 1.0
+ */
 @RestController
 public class ApplicationController {
 
-    @RequestMapping
+    @RequestMapping("/hello")
     public String hello() {
         return "hello";
     }
