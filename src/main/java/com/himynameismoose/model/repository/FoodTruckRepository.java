342d3dad71a8fefbb98c73b01abfb2f910dcdca2
himynameismoose <mershellemae@gmail.com>
2022-05-06 14:19:28 -0700

    Create repository to use jpa

---
+++ src/main/java/com/himynameismoose/model/repository/FoodTruckRepository.java
@@ -0,0 +1,12 @@
+package com.himynameismoose.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * This interface will use JPA to generate code for CRUD operations
+ *
+ * @author mershellerivera
+ * version 1.0
+ */
+public interface FoodTruckRepository extends JpaRepository {
+}
