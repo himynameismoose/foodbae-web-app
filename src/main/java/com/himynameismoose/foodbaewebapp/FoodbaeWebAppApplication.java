8dae2ce6dcf0ea57f86aea5a9c51f93a84c7175e
himynameismoose <mershellemae@gmail.com>
2022-05-06 14:56:47 -0700

    add notes to start application

--- src/main/java/com/himynameismoose/foodbaewebapp/FoodbaeWebAppApplication.java
+++ src/main/java/com/himynameismoose/foodbaewebapp/FoodbaeWebAppApplication.java
@@ -6,6 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * This is the starting point of the application
+ *
+ * @author mershellerivera
+ * @version 1.0
+ */
 @SpringBootApplication
 @RestController
 public class FoodbaeWebAppApplication {
