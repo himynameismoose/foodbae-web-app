91932f10be1e17b2a8b5d21344dbfc962b2d8619
himynameismoose <mershellemae@gmail.com>
2022-05-08 14:25:56 -0700

    add column notation

--- src/main/java/com/himynameismoose/foodtruckwebapp/model/FoodTruck.java
+++ src/main/java/com/himynameismoose/foodtruckwebapp/model/FoodTruck.java
@@ -1,23 +1,36 @@
-package com.himynameismoose.model;
+package com.himynameismoose.foodtruckwebapp.model;
 
 import javax.persistence.*;
 
 /**
  * This class will represent each Food Truck in San Francisco.
+ *
+ * @author mershellerivera
+ * @version 1.0
  */
 @Entity
-@Table(name = "foodtruck")
+@Table(name = "foodtrucks")
 public class FoodTruck {
 
     // Details of Food Truck
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(name = "name")
     private String name;
+    @Column(name = "address")
     private String address;
+    @Column(name = "food")
     private String food;
 
     public FoodTruck() {
+    }
+
+    public FoodTruck(int id, String name, String address, String food) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.food = food;
     }
 
     public int getId() {
