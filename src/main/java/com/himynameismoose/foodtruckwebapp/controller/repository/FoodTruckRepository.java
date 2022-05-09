6b1be1e45014dd78f0c83e5e44f396cefb362369
himynameismoose <mershellemae@gmail.com>
2022-05-08 21:58:24 -0700

    remove repository notation

--- src/main/java/com/himynameismoose/foodtruckwebapp/controller/repository/FoodTruckRepository.java
+++ src/main/java/com/himynameismoose/foodtruckwebapp/controller/repository/FoodTruckRepository.java
@@ -2,7 +2,6 @@ package com.himynameismoose.foodtruckwebapp.controller.repository;
 
 import com.himynameismoose.foodtruckwebapp.model.FoodTruck;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 /**
  * This interface will use JPA to generate code for CRUD operations
@@ -10,6 +9,7 @@ import org.springframework.stereotype.Repository;
  * @author mershellerivera
  * version 1.0
  */
-@Repository // collection of objects -> Data
+
 public interface FoodTruckRepository extends JpaRepository<FoodTruck, Integer> {
+    // All CRUD methods
 }
