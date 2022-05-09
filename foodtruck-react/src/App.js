8ee0ee248647c39f56243bb08966f4522c9632e9
himynameismoose <mershellemae@gmail.com>
2022-05-09 00:55:05 -0700

    configure routing

--- foodtruck-react/src/App.js
+++ foodtruck-react/src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import FooterComponent from './components/FooterComponent';
 import HeaderComponent from './components/HeaderComponent';
 import ListFoodTruckComponent from './components/ListFoodTruckComponent';
 
 function App() {
     return (
-        <div >
-            <HeaderComponent/>
-            <ListFoodTruckComponent/>
-            <FooterComponent/>
+        <div>
+            <Router>
+                <HeaderComponent/>
+                <div className="container">
+                    <Routes>
+                        <Route exact path="/" element={<ListFoodTruckComponent/>}></Route>
+                        <Route path="/foodtrucks" element={<ListFoodTruckComponent/>}></Route>
+                    </Routes>
+                </div>
+                <FooterComponent/>
+            </Router>
         </div>
     );
 }
