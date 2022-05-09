c1f89048fe2698cfcd8de66c9fc4188724e1cbec
himynameismoose <mershellemae@gmail.com>
2022-05-09 12:48:30 -0700

    add method to build database with .csv

--- src/main/java/com/himynameismoose/foodtruckwebapp/FoodTruckWebAppApplication.java
+++ src/main/java/com/himynameismoose/foodtruckwebapp/FoodTruckWebAppApplication.java
@@ -2,10 +2,17 @@ package com.himynameismoose.foodtruckwebapp;
 
 import com.himynameismoose.foodtruckwebapp.controller.repository.FoodTruckRepository;
 import com.himynameismoose.foodtruckwebapp.model.FoodTruck;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.exceptions.CsvException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
 
 /**
  * This is the starting point of the application.
@@ -17,6 +24,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FoodTruckWebAppApplication implements CommandLineRunner {
 
+	public final String CSVFile = "src/main/resources/San Francisco Food Trucks - Mobile_Food_Facility_Permit.csv";
 	public static void main(String[] args) {
 		SpringApplication.run(FoodTruckWebAppApplication.class, args);
 	}
@@ -24,13 +32,37 @@ public class FoodTruckWebAppApplication implements CommandLineRunner {
 	@Autowired
 	private FoodTruckRepository repository;
 
+	// this method will build the database using the .csv file
+	public void buildDB(String file) {
+
+		try {
+			FileReader fr = new FileReader(file);
+			CSVReader csvReader = new CSVReaderBuilder(fr)
+					.withSkipLines(1)
+					.build();
+
+			List<String[]> allData = csvReader.readAll();
+
+			for (String[] row : allData) {
+
+				String name = row[0];
+				String address = row[2];
+				String food = row[4];
+
+				FoodTruck foodtruck = new FoodTruck();
+				foodtruck.setName(name);
+				foodtruck.setAddress(address);
+
+				repository.save(foodtruck);
+			}
+		} catch (IOException | CsvException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
 	@Override
 	public void run(String... args) throws Exception {
-		// test: add to the database
-//		FoodTruck foodtruck = new FoodTruck();
-//		foodtruck.setName("Taco Truck");
-//		foodtruck.setAddress("100 ave");
-//		foodtruck.setFood("Tacos");
-//		repository.save(foodtruck);
+
+		buildDB(CSVFile);
 	}
 }
