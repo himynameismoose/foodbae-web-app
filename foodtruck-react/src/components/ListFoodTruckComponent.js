0abd4ec269b2a90f5046c5b3282607552c0661c2
himynameismoose <mershellemae@gmail.com>
2022-05-09 00:00:04 -0700

    connect react to api

--- foodtruck-react/src/components/ListFoodTruckComponent.js
+++ foodtruck-react/src/components/ListFoodTruckComponent.js
@@ -1,33 +1,45 @@
-import React, { useState} from 'react'
+import React, { useState, useEffect } from 'react'
+import FoodTruckService from '../services/FoodTruckService'
 
 const ListFoodTruckComponent = () => {
 
     const [foodtrucks, setFoodTrucks] = useState([])
 
-  return (
-    <div className="container">
-        <h2 className="text-center"> List Food Trucks </h2>
-        <table className="table table-bordered table-striped"> 
-            <thead>
+    useEffect(() => {
+
+        FoodTruckService.getAllFoodTrucks().then((response) => {
+            setFoodTrucks(response.data);
+            console.log(response.data);
+        }).catch(error => {
+            console.log(error);
+        })
+    }, [])
+
+
+    return (
+        <div className="container">
+            <h2 className="text-center"> List Food Trucks </h2>
+            <table className="table table-bordered table-striped">
+                <thead>
                 <th>Name</th>
                 <th>Address</th>
                 <th>Food</th>
-            </thead>
-            <tbody>
+                </thead>
+                <tbody>
                 {
                     foodtrucks.map(
                         foodtruck =>
-                        <tr key={foodtruck.id}>
-                            <td> {foodtruck.name} </td>
-                            <td> {foodtruck.address} </td>
-                            <td> {foodtruck.food} </td>
-                        </tr>
+                            <tr key={foodtruck.id}>
+                                <td> {foodtruck.name} </td>
+                                <td> {foodtruck.address} </td>
+                                <td> {foodtruck.food} </td>
+                            </tr>
                     )
                 }
-            </tbody>
-        </table>
-    </div>
-  )
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 export default ListFoodTruckComponent
